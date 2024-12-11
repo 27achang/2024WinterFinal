beadfb7f50edfd9aa447e66f91a3681dfe33ad39
27achang <179789901+27achang@users.noreply.github.com>
2024-12-11 08:03:21 -0800

    Add ANSI color codes

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,6 +1,26 @@
 import java.util.Scanner;
 
 public class Simulation {
+    public static final String ANSI_RESET = "\u001B[0m";
+    
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
     private Scanner input;
 
     Simulation() {
@@ -8,9 +28,9 @@ public class Simulation {
 
         // Collect user details
         System.out.println("""
-                ----------------------------------
-                  Welcome to the adventure game!
-                ----------------------------------
+                ---------------------------
+                  Welcome to the Clue 2.0!
+                ---------------------------
                 """);
     }
 
