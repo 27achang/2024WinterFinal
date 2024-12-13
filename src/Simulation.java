896a8c6f84b5eeb7077019ee82deef05bcb2addf
27achang <179789901+27achang@users.noreply.github.com>
2024-12-13 14:28:50 -0800

    Update promptInput error processing

--- src/Simulation.java
+++ src/Simulation.java
@@ -33,8 +33,7 @@ public class Simulation {
           Welcome to Clue 2.0!
         -----------------------""");
 
-        System.out.println("Please select from the options below:");
-        String command = promptInput(true,
+        String command = promptInput("Please select from the options below:", true,
             Command.ABOUT,
             Command.BEGIN
         );
@@ -53,56 +52,77 @@ public class Simulation {
     /**
      * Prompts the user for input with a choice from the given commands and with rolling printing if {@code rolling} is true
      * 
+     * @param message the message with which to prompt the user
      * @param rolling whether the print to System output should be rolling
      * @param commands the commands to list and accept as valid input
      * @return the command selected by the user
-     * @throws InterruptedException
      */
-    public String promptInput(boolean rolling, Command... commands) {
+    public String promptInput(String message, boolean rolling, Command... commands) {
         String answer;
         ArrayList<String> acceptedCommands = new ArrayList<>();
-        do {
+        if(rolling) rollingPrintln(message);
+        else System.out.println(message);
+        for(Command command : commands) {
+            if(rolling) rollingPrintln(ANSI_BLUE_BACKGROUND + ANSI_BLACK + " " + command.getName() + " " + ANSI_RESET + " - " + command.getDescription());
+            else System.out.println(ANSI_BLUE_BACKGROUND + ANSI_BLACK + " " + command.getName() + " " + ANSI_RESET + " - " + command.getDescription());
+            acceptedCommands.add(command.getName());
+            if(command.containsAliases()) Arrays.stream(command.getAliases()).forEach((alias) -> acceptedCommands.add(alias));
+        }
+        System.out.print("> ");
+        answer = input.nextLine();
+        while (!acceptedCommands.contains(answer)) {
+            System.out.print("\033[H\033[2J");
+            if(rolling){
+                rollingPrintln("You can't do that right now.");
+                rollingPrintln(message);
+            } else {
+                System.out.println("You can't do that right now.");
+                System.out.println(message);
+            }
             for(Command command : commands) {
-                if(rolling){
-                    try {
-                        rollingPrintln(ANSI_BLUE_BACKGROUND + ANSI_BLACK + " " + command.getName() + " " + ANSI_RESET + " - " + command.getDescription());
-                    } catch(InterruptedException e) {
-                        System.out.println(ANSI_BLUE_BACKGROUND + ANSI_BLACK + " " + command.getName() + " " + ANSI_RESET + " - " + command.getDescription());
-                    }
-                } else System.out.println(ANSI_BLUE_BACKGROUND + ANSI_BLACK + " " + command.getName() + " " + ANSI_RESET + " - " + command.getDescription());
+                if(rolling) rollingPrintln(ANSI_BLUE_BACKGROUND + ANSI_BLACK + " " + command.getName() + " " + ANSI_RESET + " - " + command.getDescription());
+                else System.out.println(ANSI_BLUE_BACKGROUND + ANSI_BLACK + " " + command.getName() + " " + ANSI_RESET + " - " + command.getDescription());
                 acceptedCommands.add(command.getName());
                 if(command.containsAliases()) Arrays.stream(command.getAliases()).forEach((alias) -> acceptedCommands.add(alias));
             }
             System.out.print("> ");
             answer = input.nextLine();
-            //!Arrays.stream(commands).anyMatch((a) -> a.matches(answer))
-        } while(!acceptedCommands.contains(answer));
+        }
         final String finalAnswer = answer;
         return ((Command)(Arrays.stream(commands).filter((a) -> a.matches(finalAnswer)).toArray()[0])).getName();
     }
 
     public boolean isInvalidCommand(String givenCommand, Command[] commands) {
-        for(Command command : commands){
+        for (Command command : commands){
             if(command.matches(givenCommand)) return true;
         }
         return false;
     }
 
-    public static void rollingPrint(Object obj) throws InterruptedException {
-        String output = obj.toString();
-        for(int i = 0; i < output.length() - 1; i++) {
-            System.out.print(output.charAt(i));
-            Thread.sleep(15);
+    public static void rollingPrint(Object obj) {
+        try {
+            String output = obj.toString();
+            for(int i = 0; i < output.length() - 1; i++) {
+                System.out.print(output.charAt(i));
+                Thread.sleep(15);
+            }
+            System.out.print(output.charAt(output.length() - 1));
+        } catch (InterruptedException e) {
+            System.out.print(obj);
         }
-        System.out.print(output.charAt(output.length() - 1));
     }
 
-    public static void rollingPrintln(Object obj) throws InterruptedException {
-        String output = obj.toString();
-        for(int i = 0; i < output.length() - 1; i++) {
-            System.out.print(output.charAt(i));
+    public static void rollingPrintln(Object obj) {
+        try {
+            String output = obj.toString();
+            for (int i = 0; i < output.length() - 1; i++) {
+                System.out.print(output.charAt(i));
+                Thread.sleep(15);
+            }
+            System.out.println(output.charAt(output.length() - 1));
+        } catch (InterruptedException e) {
+            System.out.println(obj);
         }
-        System.out.println(output.charAt(output.length() - 1));
     }
 
     public static void main(String[] args) throws Exception {
